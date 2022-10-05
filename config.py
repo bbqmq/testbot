7ecc92f3fd8d419bd1387586efb8ca8ee94d4041
bbqmq <114133846+bbqmq@users.noreply.github.com>
2022-10-05 22:29:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Слито в https://t.me/HACKER_PHONE_VIP
+
+Admin = "https://t.me/bbqmq" # Ссылка на админа
+token = "5682075928:AAE6nCCtcRbn03vRp7y0yuTuZ-05w0rO0R8" # Токен бота с @BotFather
+
+qiwi = "https://qiwi.com/n/NEXXLF" # Ваш QIWI
+payeer = "P1080814310" # Ваш PAYEER
+
+# Слито в https://t.me/HACKER_PHONE_VIP
